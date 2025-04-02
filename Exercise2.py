e68656ccb1473557ae8b59ee968152c962894d78
xAspro <sooryabalan.m@tifr.res.in>
2025-04-02 15:32:39 +0530

    Update April 2

--- Exercise2.py
+++ Exercise2.py
@@ -12,19 +12,19 @@ import numpy as np
 
 
 # Data
-ID = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
+id = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
 x = np.array([201, 244, 47, 287, 203, 58, 210, 202, 198, 158, 165, 201, 157, 131, 166, 160, 186, 125, 218, 146])
 y = np.array([592, 401, 583, 402, 495, 173, 479, 504, 510, 416, 393, 442, 317, 311, 400, 337, 423, 334, 533, 344])
 sigy = np.array([61, 25, 38, 15, 21, 15, 27, 14, 30, 16, 14, 25, 52, 16, 34, 31, 42, 26, 16, 22])
 sigx = np.array([9, 4, 11, 7, 5, 9, 4, 4, 11, 7, 5, 5, 5, 6, 6, 5, 9, 8, 6, 5])
 rhoxy = np.array([-0.84, 0.31, 0.64, -0.27, -0.33, 0.67, -0.02, -0.05, -0.84, -0.69, 0.30, -0.46, -0.03, 0.50, 0.73, -0.52, 0.90, 0.40, -0.78, -0.56])
 
-ID = ID.reshape(-1, 1)
+id = id.reshape(-1, 1)
 x = x.reshape(-1, 1)
 y = y.reshape(-1, 1)
 sigy = sigy
 
-# print(ID)
+# print(id)
 # print(x)
 # print(y)
 # print(sigy)
@@ -55,7 +55,7 @@ db, dm = np.sqrt(np.diag(cov_matrix))
 
 
 # Print the equation
-print(f"y = ({b:.2f} +/- {db:.2f})x + ({m:.2f} +/- {dm:.2f})")
+print(f"\ny = ({m:.2f} ± {dm:.2f})x + ({b:.0f} ± {db:.0f})\n")
 
 # Plot the data and the fit
 
@@ -67,7 +67,7 @@ ylim_max = 700
 plt.errorbar(x.flatten(), y.flatten(), yerr=sigy, fmt='o', capsize=3, capthick=2, label='Data')
 x_fit = np.linspace(xlim_min, xlim_max, 100)
 y_fit = b + m * x_fit
-plt.plot(x_fit, y_fit, label=f'Fit: y = ({m:.2f} +/- {dm:.2f}) x + ({b:.0f} +/- {db:.0f})')
+plt.plot(x_fit, y_fit, label=f'Fit: y = ({m:.2f} ± {dm:.2f}) x + ({b:.0f} ± {db:.0f})')
 
 plt.xlabel('x')
 plt.ylabel('y')
--- Exercise3.py
+++ Exercise3.py
@@ -12,19 +12,19 @@ import numpy as np
 
 
 # Data
-ID = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
+id = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
 x = np.array([201, 244, 47, 287, 203, 58, 210, 202, 198, 158, 165, 201, 157, 131, 166, 160, 186, 125, 218, 146])
 y = np.array([592, 401, 583, 402, 495, 173, 479, 504, 510, 416, 393, 442, 317, 311, 400, 337, 423, 334, 533, 344])
 sigy = np.array([61, 25, 38, 15, 21, 15, 27, 14, 30, 16, 14, 25, 52, 16, 34, 31, 42, 26, 16, 22])
 sigx = np.array([9, 4, 11, 7, 5, 9, 4, 4, 11, 7, 5, 5, 5, 6, 6, 5, 9, 8, 6, 5])
 rhoxy = np.array([-0.84, 0.31, 0.64, -0.27, -0.33, 0.67, -0.02, -0.05, -0.84, -0.69, 0.30, -0.46, -0.03, 0.50, 0.73, -0.52, 0.90, 0.40, -0.78, -0.56])
 
-ID = ID[4:].reshape(-1, 1)
+id = id[4:].reshape(-1, 1)
 x = x[4:].reshape(-1, 1)
 y = y[4:].reshape(-1, 1)
 sigy = sigy[4:]
 
-# print(ID)
+# print(id)
 # print(x)
 # print(y)
 # print(sigy)
@@ -57,7 +57,7 @@ db, dm, dq = np.sqrt(np.diag(cov_matrix))
 
 
 # Print the equation
-print(f"y = ({q:.4f} +/- {dq:.4f}) x^2 + ({b:.2f} +/- {db:.2f})x + ({m:.2f} +/- {dm:.2f})")
+print(f"\ny = ({q:.4f} ± {dq:.4f}) x² + ({m:.2f} ± {dm:.2f})x + ({b:.0f} ± {db:.0f})\n")
 
 # Plot the data and the fit
 
@@ -69,7 +69,7 @@ ylim_max = 700
 plt.errorbar(x.flatten(), y.flatten(), yerr=sigy, fmt='o', capsize=3, capthick=2, label='Data')
 x_fit = np.linspace(xlim_min, xlim_max, 100)
 y_fit = b + m * x_fit + q * x_fit**2
-plt.plot(x_fit, y_fit, label=f'Fit: y = ({q:.4f} +/- {dq:.4f}) x^2 + ({m:.2f} +/- {dm:.2f}) x + ({b:.0f} +/- {db:.0f})')
+plt.plot(x_fit, y_fit, label=f'Fit: y = ({q:.4f} ± {dq:.4f}) x$^2$ + ({m:.2f} ± {dm:.2f}) x + ({b:.0f} ± {db:.0f})')
 
 plt.xlabel('x')
 plt.ylabel('y')
